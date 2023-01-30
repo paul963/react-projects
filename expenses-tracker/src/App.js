e3327937976011b5eb8d5926eeff79a08fa938b3
Paul Ionascut <paul.ionascut@gmail.com>
2023-01-30 18:14:45 +0200

    Create dummyExpenses.js file

--- expenses-tracker/src/App.js
+++ expenses-tracker/src/App.js
@@ -2,33 +2,7 @@ import React, { useState } from 'react';
 
 import Expenses from './components/Expenses/Expenses.js';
 import NewExpense from './components/NewExpense/NewExpense.js';
-
-const dummy_expenses = [
-    {
-        id: 'e1',
-        title: 'Paper',
-        amount: 94.12,
-        date: new Date(2023, 7, 14),
-    },
-    {
-        id: 'e2',
-        title: 'TV',
-        amount: 799.49,
-        date: new Date(2023, 2, 12)
-    },
-    {
-        id: 'e3',
-        title: 'Car Insurance',
-        amount: 294.67,
-        date: new Date(2021, 2, 28),
-    },
-    {
-        id: 'e4',
-        title: 'Desk',
-        amount: 450,
-        date: new Date(2021, 5, 12),
-    },
-];
+import { dummy_expenses } from './components/Expenses/DummyExpenses';
 
 const App = () => {
     const [expenses, setExpenses] = useState(dummy_expenses);
---
+++ expenses-tracker/src/components/Expenses/DummyExpenses.js
@@ -0,0 +1,26 @@
+const dummy_expenses = [
+    {
+        id: 'e1',
+        title: 'Paper',
+        amount: 94.12,
+        date: new Date(2023, 7, 14),
+    },
+    {
+        id: 'e2',
+        title: 'TV',
+        amount: 799.49,
+        date: new Date(2023, 2, 12)
+    },
+    {
+        id: 'e3',
+        title: 'Car Insurance',
+        amount: 294.67,
+        date: new Date(2021, 2, 28),
+    },
+    {
+        id: 'e4',
+        title: 'Desk',
+        amount: 450,
+        date: new Date(2021, 5, 12),
+    },
+];
